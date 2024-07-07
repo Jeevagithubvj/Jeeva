9d256eb591784aae512e2d89c923418a86b32674
Jeevagithubvj <150387261+Jeevagithubvj@users.noreply.github.com>
2024-07-08 03:29:16 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello JEEVA");
-            System.out.println("Hello Vijay");
-                System.out.println("Hello Vijay");
+            
                    
     }
 }
